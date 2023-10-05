c8ac4916c74414ea48e9bd2c2749f060f8f6e281
William <williamfs133@gmail.com>
2023-10-05 09:53:04 -0300

    Projeto finalizado \o/

--- src/components/RestaurantList/styles.ts
+++ src/components/RestaurantList/styles.ts
@@ -3,14 +3,28 @@ import { BreakPoint, Cores } from '../../styles';
 
 export const RestaurantContainer = styled.ul`
 	display: grid;
-	grid-template-columns: repeat(2, 1fr);
+
 	grid-gap: 80px 40px;
 	padding-top: 80px;
 	padding-bottom: 120px;
 	background-color: ${Cores.bgColor};
 	padding-bottom: 120px;
 
-	@media (max-width: ${BreakPoint.tablet}) {
+	@media (max-width: 767px) {
 		grid-template-columns: 1fr;
+		margin: 0 auto;
+		max-width: 472px;
+	}
+
+	@media (min-width: ${BreakPoint.tablet}) {
+		max-width: 472px;
+		grid-template-columns: 1fr;
+		margin: 0 auto;
+	}
+
+	@media (min-width: ${BreakPoint.desktop}) {
+		grid-template-columns: repeat(2, 1fr);
+		max-width: 100%;
+		width: 100%;
 	}
 `;
